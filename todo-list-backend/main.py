c466d57f56d702a217811799689f8a52024ad041
Arqile Dhamo <arkiledamo97@gmail.com>
2024-06-29 20:22:55 +0300

    Create and delete task

--- todo-list-backend/main.py
+++ todo-list-backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -15,16 +16,27 @@ app.add_middleware(
 class Task(BaseModel):
     id: int
     text: str
+    status: str
 
-tasks = [
-    {"id": 1, "text": "Task 1"},
-    {"id": 2, "text": "Task 2"},
-]
+# creates an empty array and waits for tasks
+tasks: List[Task] = []
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+# get tasks
 @app.get("/tasks")
 def read_tasks():
     return tasks
+
+# post tasks
+@app.post("/tasks", response_model=Task)  # specify response model to return the created task
+def create_task(task: Task):
+    tasks.append(task)
+    return task  # return the created task
+
+# delete tasks in route with task_id ex /tasks/1
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    task_to_delete = next((task for task in tasks if task.id == task_id), None)
+    if task_to_delete is None:
+        raise HTTPException(status_code=404, detail="Task not Found")
+    tasks.remove(task_to_delete)
+    return task_to_delete
